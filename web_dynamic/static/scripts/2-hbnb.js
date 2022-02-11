36831a5c831df66f1c8adb72cc62cdcab59c14d9
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-10 21:34:57 -0500

    Coded 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,20 @@
+$('document').ready(function () {
+    const amenitids = {};
+    $('input[type="checkbox"]').change(function () {
+        if ($(this).is(':checked')) {
+            amenitids[$(this).attr('data-id')] = $(this).attr('data-name');
+        }
+        else {
+            delete amenitids[$(this).attr('data-id')];
+        }
+        $('div.amenities h4').text(Object.values(amenitids).join(', '));
+    })
+  
+    $.get("http://localhost:5001/api/v1/status/", function(data) {
+      if (data.status === "OK") {
+        $("#api_status").addClass("available")
+      } else {
+        $("#api_status").removeClass("available")
+      }
+    })
+  });
